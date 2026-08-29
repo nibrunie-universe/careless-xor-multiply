b4a97c3eaf1aab6069c8cbf43825c973c2a015db
Nicolas Brunie <nibrunie@gmail.com>
2026-08-29 09:33:37 -0700

    Adding carry-less divide

--- test/test_carry_less_multiply.py
+++ test/test_carry_less_multiply.py
@@ -1,7 +1,18 @@
-from carry_less_multiply import carry_less_multiply
+from carry_less_multiply import carry_less_multiply, carry_less_divide
 
 def test_carry_less_multiply():
     assert carry_less_multiply(0, 0) == 0
     assert carry_less_multiply(1, 1) == 1
     assert carry_less_multiply(3, 2) == 6
     assert carry_less_multiply(3, 3) == 5 # (X + 1) * (X +1) = X^2 + 2 X + 1, since 2 congruent to 0 
+
+
+def test_carry_less_divider():
+    assert carry_less_divide(17, 0) == None
+    assert carry_less_divide(3, 17) == 0
+    assert carry_less_divide(8, 2) == 4
+    quotient = 17
+    divisor = 37
+    clmul_result = carry_less_multiply(quotient, divisor)
+    assert carry_less_divide(clmul_result, divisor) == quotient
+    assert carry_less_divide(clmul_result ^ (divisor >> 1), divisor) == quotient
