b13463473624b45f5762550107b1f0fffbea0810
Nicolas Brunie <nibrunie@gmail.com>
2026-08-29 09:18:59 -0700

    Adding bit manip lzc (leading zero count) function and associated unit test

--- test/test_bit_manip_utils.py
+++ test/test_bit_manip_utils.py
@@ -1,4 +1,4 @@
-from bit_manip_utils import bit_reverse, byte_split
+from bit_manip_utils import bit_reverse, byte_split, lzc
 
 def test_bit_reverse():
     assert bit_reverse(0b1010, 4) == 0b0101
@@ -8,3 +8,11 @@ def test_bit_reverse():
 def test_byte_split():
     assert byte_split(0x12345678) == [0x78, 0x56, 0x34, 0x12]
     assert byte_split(0x0) == [0]
+
+
+def test_lzc():
+    assert lzc(1, 1) == 0
+    assert lzc(1, 17) == 16
+    assert lzc(0, 17) == 17
+    assert lzc(3, 3) == 1
+    assert lzc (8, 5) == 1 
