beae415713b1bf1b3c0d861600e9f85e0ad5cab3
Nicolas Brunie <nibrunie@gmail.com>
2026-08-29 09:45:50 -0700

    Adding carry_less_remainder and associated sanity unit test

--- test/test_carry_less_multiply.py
+++ test/test_carry_less_multiply.py
@@ -1,4 +1,4 @@
-from carry_less_multiply import carry_less_multiply, carry_less_divide
+from carry_less_multiply import carry_less_multiply, carry_less_divide, carry_less_remainder
 
 def test_carry_less_multiply():
     assert carry_less_multiply(0, 0) == 0
@@ -16,3 +16,14 @@ def test_carry_less_divider():
     clmul_result = carry_less_multiply(quotient, divisor)
     assert carry_less_divide(clmul_result, divisor) == quotient
     assert carry_less_divide(clmul_result ^ (divisor >> 1), divisor) == quotient
+
+
+def test_carry_less_remainder():
+    assert carry_less_remainder(17, 0) == 17
+    assert carry_less_remainder(3, 17) == 3
+    assert carry_less_remainder(8, 2) == 0
+    quotient = 17
+    divisor = 37
+    clmul_result = carry_less_multiply(quotient, divisor)
+    assert carry_less_remainder(clmul_result, divisor) == 0
+    assert carry_less_remainder(clmul_result ^ (divisor >> 1), divisor) == (divisor >> 1)
