7416a2b57f6277017a957dd894cd1d9f7fc94295
Nicolas Brunie <82109999+nibrunieAtSi5@users.noreply.github.com>
2026-08-30 09:59:03 -0700

    [Testing] Moving crc_model tests into examples/crc/test and introducing unit tests for clmul based crc32(c) (#8)

--- test/test_carry_less_multiply.py
+++ test/test_carry_less_multiply.py
@@ -1,4 +1,7 @@
+import random
+
 from carry_less_multiply import carry_less_multiply, carry_less_divide, carry_less_remainder
+from bit_manip_utils import bit_reverse
 
 def test_carry_less_multiply():
     assert carry_less_multiply(0, 0) == 0
@@ -27,3 +30,17 @@ def test_carry_less_remainder():
     clmul_result = carry_less_multiply(quotient, divisor)
     assert carry_less_remainder(clmul_result, divisor) == 0
     assert carry_less_remainder(clmul_result ^ (divisor >> 1), divisor) == (divisor >> 1)
+
+
+def test_carry_less_multiply_reverse():
+    # carry-less multiply has an interesting property
+    # for a n-bit operand A and a m-bit operand B
+    #    bit_reverse(clmul(A,B), n+m-1) = clmul(bit_reverse(A, n), bit_reverse(B, m))
+    # this can be rewritten as: 
+    #    bit_reverse(clmul(A,B), n+m) = (clmul(bit_reverse(A, n), bit_reverse(B, m))) << 1
+    a = random.randrange(2**32)
+    b = random.randrange(2**32)
+    rev64 = lambda v: bit_reverse(v, 64)
+    rev32 = lambda v: bit_reverse(v, 32)
+    assert rev64(a << 32) == rev32(a)
+    assert rev64(carry_less_multiply(a, b)) == (carry_less_multiply(rev32(a), rev32(b)) << 1)
