c16beb27ef76cba8dcd9cc89881ccfd9d16df9af
Nicolas Brunie <nibrunie@gmail.com>
2026-08-29 20:41:21 -0700

    Adding CRC32C specific carry-less multiply based implementation + more testing

--- examples/crc/crc32_le_clmul.py
+++ examples/crc/crc32_le_clmul.py
@@ -1,14 +1,26 @@
-from carry_less_multiply import carry_less_multiply
+from bit_manip_utils import byte_split
+from crc_model import CRC32C_POLY_LE
+from carry_less_multiply import carry_less_multiply, carry_less_divide
 import random
 
 from bit_manip_utils import bit_reverse, byte_assemble
-from crc_model import CRC32_POLY_LE, crc32_le, crc32_be, CRC32_POLY_BE
-from crc_intro import CRC32_BE_INV, crc32_be_clmul
+from crc_model import CRC32_POLY_LE, crc32_le, crc32c, CRC32C_POLY_LE
+from crc_intro import CRC32_BE_INV
 
 CRC32_BE_INV_REV = bit_reverse(CRC32_BE_INV, 32)
 
 def crc32_le_clmul(rev_data: int) -> int:
+    return crc32_le_clmul_generic(rev_data, CRC32_POLY_LE)
+
+def crc32_le_clmul_generic(rev_data: int, poly_le: int) -> int:
     assert rev_data < 2**32
+    # building normal form poly from reverse (le) polynomial
+    poly_be = bit_reverse(poly_le, 32)
+    # computing normal form barrett's coefficient X^63 / poly
+    poly_inv = carry_less_divide(1 << 63, (1 << 32) ^ poly_be)
+    # computing reverse form barrett's coefficient
+    poly_inv_rev = bit_reverse(poly_inv, 32)
+
     # Theory for CRC32BE:
     # data.X^32 = Q*P ^ crc
     # data.CRC32_BE_INV.x^32 = Q.(P.CRC32_BE_INV) ^ crc
@@ -42,7 +54,7 @@ def crc32_le_clmul(rev_data: int) -> int:
     # rev64(data . CRC32_BE_INV) >> 1 = (rev32(Q)) ^ (rev31(R31) << 32)
     # rev32(data) . CRC32_BE_INV_REV  = (rev32(Q)) ^ (rev31(R31) << 32)
     # rev32(Q) corresponds to the least 32 bits of rev32(data) . CRC32_BE_INV_REV
-    rev32_q = carry_less_multiply(rev_data, CRC32_BE_INV_REV) & 0xffffffff
+    rev32_q = carry_less_multiply(rev_data, poly_inv_rev) & 0xffffffff
     print(f"rev32_q({hex(rev_data)}) = {hex(rev32_q)}")
 
     # computing the remainder
@@ -52,8 +64,39 @@ def crc32_le_clmul(rev_data: int) -> int:
     #
     #    rev64(Q.P) = rev32(Q) ^ (rev32(Q).CRC32_POLY_LE) << 1 
     #    rev64(Q.P) >> 32 = (rev32(Q).CRC32_POLY_LE) >> 31 
-    remainder_rev = carry_less_multiply(rev32_q, CRC32_POLY_LE) >> 31
+    remainder_rev = carry_less_multiply(rev32_q, poly_le) >> 31
     return remainder_rev
+
+def crc32c_clmul(rev_data: int) -> int:
+    assert rev_data < 2**32
+    # the polynomial forms can be statically evaluated
+    # building normal form poly from reverse (le) polynomial
+    poly_be = bit_reverse(CRC32C_POLY_LE, 32)
+    # computing normal form barrett's coefficient X^63 / poly
+    poly_inv = carry_less_divide(1 << 63, (1 << 32) ^ poly_be)
+    # computing reverse form barrett's coefficient
+    poly_inv_rev = bit_reverse(poly_inv, 32)
+    assert poly_inv_rev < 2**32
+
+    # computing the quotient (see crc32_le_clmul_generic for formula details)
+    rev32_q = carry_less_multiply(rev_data, poly_inv_rev) & 0xffffffff
+    # computing the remainder (a.k.a. CRC), (see crc32_le_clmul_generic for formula details)
+    remainder_rev = carry_less_multiply(rev32_q, CRC32C_POLY_LE) >> 31
+
+    # CRC32C_POLY_LE = 0x82f63b78
+    # (rev32(Q) . CRC32C_POLY_LE) >> 31 = rev32(Q) . (CRC32C_POLY_LE . X) >> 32
+    #
+    # The issue is that (CRC32C_POLY_LE . X) exceeds 32 bits
+    # but can be exposed as (X^32 ^ ((CRC32_POLY_LE << 1) & 0xffffffff) = X^32 + P'
+    # rev32(Q) . (X^32 ^ P') >> 32 = rev32(Q) ^ (rev32(Q) . P') >> 32
+    p_prime = (CRC32C_POLY_LE << 1) & 0xffffffff # can be statically evaluated
+    remainder_opt = rev32_q ^ (carry_less_multiply(rev32_q, p_prime) >> 32)
+    # and now the carry-less part directly corresponds to the upper 32-bit part of
+    # a possibly 64-bit carry-less
+
+    assert remainder_rev == remainder_opt
+
+    return remainder_opt
     
 
 
@@ -90,3 +133,32 @@ if __name__ == "__main__":
         clmul_crc32_le = crc32_le_clmul(data_rev)
         print(f"crc32_le(0x{data_rev:x} / {data_bytes_rev}) = 0x{ref_crc32_le:x} (ref) vs 0x{clmul_crc32_le:x} (clmul)")
         assert ref_crc32_le == clmul_crc32_le
+
+    # Multi-byte data (4 bytes, corresponding to the CRC width)
+    # FOR CRC32C
+    for data_bytes_rev in [[0, 0, 0, 0], [0x0, 0x0, 0x0, 0x1], [0x1, 0x0, 0x0, 0x0], [random_bytes() for _ in range(4)]]:
+        data_rev = byte_assemble(data_bytes_rev)
+
+        # First, we compute the reference CRC32_LE value of the multi byte data
+        ref_crc32c_le = crc32c(0, data_bytes_rev)
+        # Then, we compute the same value but using carry-less multiply with the barrett's constant
+        clmul_crc32c_le = crc32_le_clmul_generic(data_rev, CRC32C_POLY_LE)
+        print(f"crc32c_le(0x{data_rev:x} / {data_bytes_rev}) = 0x{ref_crc32c_le:x} (ref) vs 0x{clmul_crc32c_le:x} (clmul)")
+        assert ref_crc32c_le == clmul_crc32c_le
+
+        clmul_crc32c = crc32c_clmul(data_rev)
+        print(f"crc32c(0x{data_rev:x} / {data_bytes_rev}) = 0x{ref_crc32c_le:x} (ref) vs 0x{clmul_crc32c:x} (clmul crc32c specific)")
+        assert ref_crc32c_le == clmul_crc32c
+
+    # More agressive CRC32C testing
+    NUM_TESTS = 100000
+    for data_bytes_rev in [[random_bytes() for _ in range(4)] for _ in range(NUM_TESTS)]:
+        data_rev = byte_assemble(data_bytes_rev)
+
+        # First, we compute the reference CRC32_LE value of the multi byte data
+        ref_crc32c_le = crc32c(0, data_bytes_rev)
+        # Then, we compute the same value but using carry-less multiply with the barrett's constant
+        clmul_crc32c = crc32c_clmul(data_rev)
+
+        # checks
+        assert ref_crc32c_le == clmul_crc32c
